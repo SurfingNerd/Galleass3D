ad5e56fd85631d08231c1154f7a52e19798f2bc5
Thomas Haller <Thomas.Haller@lab10.coop>
2019-04-25 18:59:20 +0200

    deployment test

--- truffle/migrations/1_initial_migration.js
+++ truffle/migrations/1_initial_migration.js
@@ -3,7 +3,8 @@
 const Galleass =  artifacts.require("Galleass");
 const Timber = artifacts.require("Timber");
 const Fillet = artifacts.require("Fillet");
-
+const Dogger = artifacts.require("Dogger");
+const Copper = artifacts.require("Copper");
 
 module.exports = async function(deployer) {
   //deployer.deploy(Migrations);
@@ -17,9 +18,11 @@ module.exports = async function(deployer) {
   var gallAddr = Galleass.address;
   //console.log('gal: ' +gallAddr);
   var timber = await deployer.deploy(Timber, gallAddr);
-  await deployer.deploy(Fillet, gallAddr);
-  //console.log('timber: ' + Timber.address);
 
+  var fillet = await deployer.deploy(Fillet, gallAddr);
+  var dogger = await deployer.deploy(Dogger, gallAddr);
+  var copper = await deployer.deploy(Copper, gallAddr);
+  
   //console.log(timber);
   var mintWoodTransaction = await timber.mint.sendTransaction(deploymentAccount, 10);
   
