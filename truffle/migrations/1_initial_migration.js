3f031033a80dc3930be0ddb9635d3bd4a5d96e25
Thomas Haller <Thomas.Haller@lab10.coop>
2019-04-25 13:46:44 +0200

    minting timber...

--- truffle/migrations/1_initial_migration.js
+++ truffle/migrations/1_initial_migration.js
@@ -8,11 +8,22 @@ const Fillet = artifacts.require("Fillet");
 module.exports = async function(deployer) {
   //deployer.deploy(Migrations);
   //console.log('start Deploy...');
-  await deployer.deploy(Galleass);
+  
+  
+  var accounts = await web3.eth.getAccounts();
+  var deploymentAccount = accounts[0];
+
+  await deployer.deploy(Galleass, 'Thomas Haller');
   var gallAddr = Galleass.address;
   //console.log('gal: ' +gallAddr);
-  await deployer.deploy(Timber, gallAddr);
+  var timber = await deployer.deploy(Timber, gallAddr);
   await deployer.deploy(Fillet, gallAddr);
   //console.log('timber: ' + Timber.address);
+
+  //console.log(timber);
+  var mintWoodTransaction = await timber.mint.sendTransaction(deploymentAccount, 10);
   
+  var balance = await timber.balanceOf.call(deploymentAccount);
+  console.log('minted: ' + balance);
+
 };
