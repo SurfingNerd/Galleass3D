dc17ad1eb105620eaef4b6496c707648c2c9cc4f
Thomas Haller <Thomas.Haller@lab10.coop>
2019-04-25 11:25:10 +0200

    added Timber

---
+++ truffle/contracts/ERC677Token.sol
@@ -0,0 +1,52 @@
+pragma solidity ^0.4.11;
+
+import 'openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
+
+// adapted from https://github.com/ethereum/EIPs/issues/677
+//    big thanks to Steve Ellis
+
+contract ERC677Token is StandardToken {
+
+  event TransferAndCall(address indexed from, address indexed to, uint value, bytes data);
+
+  function transferAndCall(address _to, uint _value, bytes _data)
+    public
+    validRecipient(_to)
+    returns (bool success)
+  {
+    super.transfer(_to, _value);
+    emit TransferAndCall(msg.sender, _to, _value, _data);
+    if (isContract(_to)) {
+      contractFallback(_to, _value, _data);
+    }
+    return true;
+  }
+  function contractFallback(address _to, uint _value, bytes _data)
+    validRecipient(_to)
+    private
+  {
+    ERC677Receiver receiver = ERC677Receiver(_to);
+    require(receiver.onTokenTransfer(msg.sender, _value, _data));
+  }
+
+  function isContract(address _addr)
+    private
+    returns (bool hasCode)
+  {
+    uint length;
+    assembly { length := extcodesize(_addr) }
+    return length > 0;
+  }
+
+
+  modifier validRecipient(address _recipient) {
+    require(_recipient != address(0) && _recipient != address(this));
+    _;
+
+  }
+
+}
+
+contract ERC677Receiver {
+  function onTokenTransfer(address _sender, uint _value, bytes _data) returns (bool){}
+}
---
+++ truffle/contracts/Timber.sol
@@ -0,0 +1,48 @@
+pragma solidity ^0.4.15;
+
+/*
+
+  https://galleass.io
+  by Austin Thomas Griffith
+
+  Timber is harvested from forest tiles and used to build ships and buildings.
+
+*/
+
+import './Galleasset.sol';
+import './ERC677Token.sol';
+import 'openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
+
+contract Timber is Galleasset, MintableToken, ERC677Token {
+
+  string public constant name = "Galleass Timber";
+  string public constant symbol = "G_TIMBER";
+  uint8 public constant decimals = 0;
+
+  uint256 public constant INITIAL_SUPPLY = 0;
+
+  constructor(address _galleass) Galleasset(_galleass) public {
+    totalSupply_ = INITIAL_SUPPLY;
+  }
+
+  function galleassTransferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+    require(_to != address(0));
+    require(_value <= balances[_from]);
+    require(hasPermission(msg.sender,"transferTimber"));
+
+    balances[_from] = balances[_from].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    Transfer(_from, _to, _value);
+    return true;
+  }
+
+  function galleassMint(address _to,uint _amount) public returns (bool){
+    require(hasPermission(msg.sender,"mintTimber"));
+    totalSupply_ = totalSupply_.add(_amount);
+    balances[_to] = balances[_to].add(_amount);
+    Mint(_to, _amount);
+    Transfer(address(0), _to, _amount);
+    return true;
+  }
+
+}
