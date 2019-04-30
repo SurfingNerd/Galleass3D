857e8a48e2fb5f0ac1ad7c5c254a872f05e46d05
Thomas Haller <Thomas.Haller@lab10.coop>
2019-04-30 15:52:38 +0200

    UI Improvements

--- Assets/Scripts/TransactionDetailsHolder.cs
+++ Assets/Scripts/TransactionDetailsHolder.cs
@@ -61,6 +61,7 @@ public class TransactionDetailsHolder : MonoBehaviour
             currentDisplayedDetails = details;
             Debug.Log("Text set to " + infoText.text);
         }
+        DisplayTransactionDetails();
     }
 
     public void DisplayTransactionDetails()
