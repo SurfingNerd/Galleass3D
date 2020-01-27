553104c744a6bb9279cd63ed753d46c856e0451e
Thomas Haller <Thomas.Haller@lab10.coop>
2020-01-27 17:36:21 +0100

    Ship moves to Island

---
+++ Assets/Scripts/MoveShip.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveShip : MonoBehaviour
+{
+     UnityEngine.AI.NavMeshAgent m_navMeshAgent;
+     UnityEngine.GameObject m_target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_target = GameObject.FindWithTag("Finish");
+        m_navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        m_navMeshAgent.SetDestination(m_target.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
